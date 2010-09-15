457464420114b7a434c5fc26d002579ac1907817
liuxk99 <liuxk99@bdc341c6-17c0-11de-ac9f-1d9250355bca>
2010-09-15 02:06:39 +0000

    + format adjustment.

--- trunk/Graphics/WS/Ordinal/Ordinal.h
+++ trunk/Graphics/WS/Ordinal/Ordinal.h
@@ -14,50 +14,50 @@
 //////////////////////////////////////////////////////////////////////////
 // CNumberedWindow displays a number in its center and supports drag and drop
 class CNumberedWindow : public CWindow
-	{
+    {
 public:
-	CNumberedWindow (CWsClient* aClient, TInt aNum);
-	~CNumberedWindow ();
-	void Draw (const TRect& aRect);
-	void HandlePointerEvent (TPointerEvent& aPointerEvent);
+    CNumberedWindow(CWsClient* aClient, TInt aNum);
+    ~CNumberedWindow();
+    void Draw(const TRect& aRect);
+    void HandlePointerEvent(TPointerEvent& aPointerEvent);
 private:
-	TInt	iNumber;	// Number displayed in window
-	TPoint	iOldPos;	// Position is required for drag and drop
-	};
+    TInt iNumber; // Number displayed in window
+    TPoint iOldPos; // Position is required for drag and drop
+    };
 
 // CMainWindow is a plain window that just acts as a container for the
 // other windows
 class CMainWindow : public CWindow
-	{
+    {
 public:
-	CMainWindow (CWsClient* aClient);
-	~CMainWindow ();
-	void Draw (const TRect& aRect);
-	void HandlePointerEvent (TPointerEvent& aPointerEvent);
-	};
+    CMainWindow(CWsClient* aClient);
+    ~CMainWindow();
+    void Draw(const TRect& aRect);
+    void HandlePointerEvent(TPointerEvent& aPointerEvent);
+    };
 
 //////////////////////////////////////////////////////////////////////////
 //						Derived client class
 //////////////////////////////////////////////////////////////////////////
 
 class CExampleWsClient : public CWsClient
-	{
+    {
 public:
-	static CExampleWsClient* NewL(const TRect& aRect);
+    static CExampleWsClient* NewL(const TRect& aRect);
 private:
-	CExampleWsClient (const TRect& aRect);
-	void ConstructMainWindowL();
-	~CExampleWsClient ();
-	void RunL ();
-	void HandleKeyEventL (TKeyEvent& aKeyEvent);
+    CExampleWsClient(const TRect& aRect);
+    void ConstructMainWindowL();
+    ~CExampleWsClient();
+    void RunL();
+    void HandleKeyEventL(TKeyEvent& aKeyEvent);
 private:
-	CMainWindow*	iMainWindow;
-	CNumberedWindow* iWindow1;
-	CNumberedWindow* iWindow2;
-	CNumberedWindow* iWindow3;
-	CNumberedWindow* iWindow4;
-	CNumberedWindow* iWindow5;
-	const TRect& iRect;
-	};
+    CMainWindow* iMainWindow;
+    CNumberedWindow* iWindow1;
+    CNumberedWindow* iWindow2;
+    CNumberedWindow* iWindow3;
+    CNumberedWindow* iWindow4;
+    CNumberedWindow* iWindow5;
+    const TRect& iRect;
+    };
 
 #endif
