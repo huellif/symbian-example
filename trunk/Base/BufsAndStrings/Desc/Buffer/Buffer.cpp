b809a24455a27388b09f8b5c8532132c310871b8
liuxk99 <liuxk99@bdc341c6-17c0-11de-ac9f-1d9250355bca>
2010-09-28 02:36:29 +0000

    + 重构：提取方法，格式整理。

--- trunk/Base/BufsAndStrings/Desc/Buffer/Buffer.cpp
+++ trunk/Base/BufsAndStrings/Desc/Buffer/Buffer.cpp
@@ -7,216 +7,271 @@
 //
 // Common literal text
 //
-_LIT(KTxtHelloWorld,"Hello World!");
-_LIT(KTxtRepText,"Replacement text");
-_LIT(KTxtTBufC,"TBufC: ");
-_LIT(KTxtTPtr,"TPtr:  ");
+_LIT(KTxtHelloWorld, "Hello World!");
+_LIT(KTxtRepText, "Replacement text");
+_LIT(KTxtTBufC, "TBufC: ");
+_LIT(KTxtTPtr, "TPtr:  ");
 //
 // Common Format strings
 //
-_LIT(KCommonFormat2,"\"%S\"; ");
-_LIT(KCommonFormat3,"Descriptor at %x; Ptr()=%x; ");
-_LIT(KCommonFormat6,"\"%S\"; Ptr()=%x; Length()=%d; Size()=%d\n");
-_LIT(KCommonFormat7,"\"%S\"; Ptr()=%x; Length()=%d; Size()=%d; ");
-_LIT(KCommonFormat8,"\nMaxLength()=%d\n");
-_LIT(KCommonFormat9,"Length()=%d; Size()=%d;\n");
-_LIT(KCommonFormat10,"MaxLength()=%d\n");
+_LIT(KContentFormat, "\"%S\"; ");
+_LIT(KDesAndContentAddressFormat, "Descriptor at 0x%08x; Ptr()=0x%08x; ");
+_LIT(KContentAddressLenSizeFormat, "\"%S\"; Ptr()=0x%08x; Length()=%d; Size()=%d\n");
+_LIT(KContentAddressLenSizeFormat2, "\"%S\"; Ptr()=0x%08x; Length()=%d; Size()=%d; ");
+_LIT(KMaxLenFormat, "\nMaxLength()=%d\n");
+_LIT(KLenSizeFormat, "Length()=%d; Size()=%d;\n");
+_LIT(KMaxLenFormat2, "MaxLength()=%d\n");
 
-void show_cstr(TText *cstr)
-	{
-	// Look at the address of the C string
-	_LIT(KFormat1, "C string at %x; \n");
-	console->Printf(KFormat1, &cstr[0]);
-	}
+void show_cstr_content(TText *cstr)
+    {
+    // Look at the address of the C string
+    _LIT(KFormat1, "C string at 0x%08x; \n");
+    console->Printf(KFormat1, &cstr[0]);
+    }
 
-void show_bufc1(TBufC<16> &aBufc)
-	{
-	// Look at: 
-	//   1. Descriptor content
-	//   2. Address of descriptor
-	//   3. Address of descriptor data area
-	//   4. Length of descriptor
-	//   5. Size of descriptor
-	//         
-	// Address of descriptor data area is 
-	// different from the address of cstr but
-	// is offset 4 from the start of the 
-	// descriptor itself.
-	//
-	// Descriptor length is 12.
-	//
-	// The template parameter value defines 
-	// the length of the descriptor data area 
-	// and, therefore, governs its size 
-	// (depending on the build variant).
-	// Size of data is 24
-	console->Printf(KCommonFormat2, &aBufc);
-	console->Printf(KCommonFormat3, &aBufc, aBufc.Ptr());
-	_LIT(KFormat4,"Length()=%d; Size()=%d\n");
-	console->Printf(KFormat4, aBufc.Length(), aBufc.Size());
-	}
+void show_tbufc_content_desandcontent_address_len_size(TBufC<16> &aBufC)
+    {
+    // Look at: 
+    //   1. Descriptor content
+    //   2. Address of descriptor
+    //   3. Address of descriptor data area
+    //   4. Length of descriptor
+    //   5. Size of descriptor
+    //         
+    // Address of descriptor data area is 
+    // different from the address of cstr but
+    // is offset 4 from the start of the 
+    // descriptor itself.
+    //
+    // Descriptor length is 12.
+    //
+    // The template parameter value defines 
+    // the length of the descriptor data area 
+    // and, therefore, governs its size 
+    // (depending on the build variant).
+    // Size of data is 24
+    console->Printf(KContentFormat, &aBufC);
+    console->Printf(KDesAndContentAddressFormat, &aBufC, aBufC.Ptr());
+    _LIT(KFormat4, "Length()=%d; Size()=%d\n");
+    console->Printf(KFormat4, aBufC.Length(), aBufC.Size());
+    }
 
-void show_bufc2(TBufC<16> &aBufC)
-	{
-	_LIT(KFormat5,"\"%S\"; Length()=%d; Size()=%d\n");
-	console->Printf(KFormat5, &aBufC, aBufC.Length(), aBufC.Size());
-	}
+void show_tbufc_content_len_size(TBufC<16> &aBufC)
+    {
+    _LIT(KFormat5,"\"%S\"; Length()=%d; Size()=%d\n");
+    console->Printf(KFormat5, &aBufC, aBufC.Length(), aBufC.Size());
+    }
 
-void show_bufc3(TBufC<16> &aBufC)
-	{
-	console->Printf(KTxtTBufC);
-	console->Printf(KCommonFormat6, &aBufC, aBufC.Ptr(), aBufC.Length(),
-			aBufC.Size());
-	}
+void show_tbufc_contentaddress_len_size(TBufC<16> &aBufC)
+    {
+    console->Printf(KTxtTBufC);
+    console->Printf(KContentAddressLenSizeFormat, &aBufC, aBufC.Ptr(),
+            aBufC.Length(), aBufC.Size());
+    }
 
-void show_ptr(TPtr &aPtr)
-	{
-	console->Printf(KTxtTPtr);
-	console->Printf(KCommonFormat7, &aPtr, aPtr.Ptr(), aPtr.Length(),
-			aPtr.Size());
-	console->Printf(KCommonFormat8, aPtr.MaxLength());
-	}
+void show_tptr_content_contentaddress_len_size(TPtr &aPtr)
+    {
+    console->Printf(KTxtTPtr);
+    console->Printf(KContentAddressLenSizeFormat2, &aPtr, aPtr.Ptr(),
+            aPtr.Length(), aPtr.Size());
+    console->Printf(KMaxLenFormat, aPtr.MaxLength());
+    }
+
+void show_tbuf_content_desandcontent_address_len_size_maxlen(TBuf<16> &aBuf)
+    {
+    console->Printf(KContentFormat, &aBuf);
+    console->Printf(KDesAndContentAddressFormat, &aBuf, aBuf.Ptr());
+    console->Printf(KLenSizeFormat, aBuf.Length(), aBuf.Size());
+    console->Printf(KMaxLenFormat2, aBuf.MaxLength());
+    }
+
+void example_tbufc_init_with_cstr()
+    {
+    // Set up an area and initialize to a 
+    // C style string (including the NULL).
+    TText cstr[13] =
+        {
+        'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'
+        };
+
+    // Construct a TBufC using the NULL 
+    // terminated string in cstr to initialize
+    // it.
+    TBufC<16> bufc1(&cstr[0]);
+
+    show_cstr_content(cstr);
+    show_tbufc_content_desandcontent_address_len_size(bufc1);
+    }
+
+void example_tbufc_replace_with_lit_normal(TBufC<16> &aBufC)
+    {
+    // Cannot modify existing data but can replace
+    // it entirely using assignment operator. 
+    // The replacement text must not have a length 
+    // greater than 16
+    aBufC = KTxtRepText;
+    show_tbufc_content_len_size(aBufC);
+    }
+
+void example_tbufc_replace_with_lit_panic(TBufC<16> &/*aBufC*/)
+    {
+    // Replacing text which has a length > 16
+    // causes panic !!
+    // 
+    // Remove the "//" marks on the next two lines
+    // to see this happen
+    //_LIT(KTxtRepTextPanic,"Text replacement causes panic");
+    //bufc2 = KTxtRepTextPanic;
+
+
+    // The Des() function returns a TPtr to the
+    // TBufC.
+    // The TBufC data can be changed through 
+    // the TPtr.
+    // The maximum length of the TPtr is the 
+    // value of the TBufC template parameter,
+    // i.e. 16 
+    }
+
+void example_tbufc_modify_with_tptr(TBufC<16> &aBufC)
+    {
+    aBufC = KTxtHelloWorld;
+    show_tbufc_contentaddress_len_size(aBufC);
+
+    TPtr ptr = aBufC.Des();
+    show_tptr_content_contentaddress_len_size(ptr);
+    // Now change the TBufC data through
+    // the TPtr. This is OK provided the length
+    // of the changed data does not exceed 16.
+    //
+    // The following change deletes the last 
+    // character (the "!") and appends 
+    // the characters " & Hi".
+    //
+    // Note that the length of both the TBufC 
+    // and the TPtr reflect the changed data.
+    show_tbufc_contentaddress_len_size(aBufC);
+    _LIT(KTxtAndHi, " & Hi");
+    ptr.Delete((ptr.Length() - 1), 1);
+    ptr.Append(KTxtAndHi);
+
+    show_tptr_content_contentaddress_len_size(ptr);
+    }
+
+void example_tbuf_prompt()
+    {
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    _LIT(KTxtBasicConcepts, "\n-->TBuf basic concepts");
+    console->Printf(KTxtBasicConcepts);
+    _LIT(KTxtPressToContinue, " (press any key to continue)\n");
+    console->Printf(KTxtPressToContinue);
+    console->Getch();
+    }
+
+void show_tbuf_content_len_size_maxlen_2(TBuf<16> &aBuf)
+    {
+    console->Printf(KContentFormat, &aBuf);
+    console->Printf(KLenSizeFormat, aBuf.Length(), aBuf.Size());
+    console->Printf(KMaxLenFormat2, aBuf.MaxLength());
+    }
+
+void example_tbuf_append_tbuf(TBuf<16> &aBuf)
+    {
+    // The data can be modified
+    aBuf.Append('@');
+    show_tbuf_content_len_size_maxlen_2(aBuf);
+    }
+
+void show_tbuf_content_len_size_maxlen(TBuf<16> &aBuf)
+    {
+    console->Printf(KContentFormat, &aBuf);
+    console->Printf(KLenSizeFormat, aBuf.Length(), aBuf.Size());
+    console->Printf(KMaxLenFormat2, aBuf.MaxLength());
+    }
+
+void example_tbuf_setlength(TBuf<16> &aBuf)
+    {
+    // Length can be changed; data represented
+    // by the descriptor is now "Hel"
+    aBuf.SetLength(3);
+    show_tbuf_content_len_size_maxlen(aBuf);
+    }
+
+void example_tbuf_zero(TBuf<16> &aBuf)
+    {
+    // Length can be zeroised; no data is now 
+    // represented by the descriptor but 
+    // the maximum length is still 16
+    aBuf.Zero();
+    show_tbuf_content_len_size_maxlen(aBuf);
+    }
+
+void example_tbuf_replace_with_lit_normal(TBuf<16> &aBuf)
+    {
+    // The data can be replaced entirely 
+    // using the assignment operator.
+    // The replacement text must not have a
+    // length greater than 16.
+    aBuf = KTxtRepText;
+    show_tbuf_content_len_size_maxlen(aBuf);
+    }
+
+void example_tbuf_replace_with_lit_panic(TBuf<16> &/*aBuf*/)
+    {
+    // Replacing text which has a length > 16
+    // causes panic !!
+    // 
+    // Remove the "//" marks on the next two lines
+    // to see this happen
+    //_LIT(KTxtRepTextPanic,"Text replacement causes panic");
+    //buf = _L("Text replacement causes panic!");
+    }
 
 LOCAL_C void doExampleL()
-	{
-	// Set up an area and initialise to a 
-	// C style string (including the NULL).
-	TText cstr[13] =
-		{
-		'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'
-		};
+    {
+    example_tbufc_init_with_cstr();
 
-	// Construct a TBufC using the NULL 
-	// terminated string in cstr to initialise
-	// it.
-	TBufC<16> bufc1(&cstr[0]);
+    // If the TBufC is to hold string data on
+    // construction, use a _LIT macro.
+    TBufC<16> bufc2(KTxtHelloWorld);
+    show_tbufc_content_len_size(bufc2);
 
-	show_cstr(cstr);
+    example_tbufc_replace_with_lit_normal(bufc2);
+    example_tbufc_replace_with_lit_panic(bufc2);
+    example_tbufc_modify_with_tptr(bufc2);
 
-	show_bufc1(bufc1);
+    example_tbuf_prompt();
 
-	// If the TBufC is to hold string data on
-	// construction, use a _LIT macro.
-	TBufC<16> bufc2(KTxtHelloWorld);
+    // Construct a TBuf using a Literal
+    TBuf<16> buf(KTxtHelloWorld);
 
-	show_bufc2(bufc2);
-	// Cannot modify existing data but can replace
-	// it entirely using assignment operator. 
-	// The replacement text must not have a length 
-	// greater than 16
-	bufc2 = KTxtRepText;
-	show_bufc2(bufc2);
+    // Look at: 
+    //   1. Descriptor content
+    //   2. Address of descriptor
+    //   3. Address of descriptor data area
+    //   4. Length of descriptor
+    //   5. Size of descriptor
+    //   6. Maximum length of descriptor      
+    //
+    // Like a TBufC, the address of the descriptor
+    // data area is offset 4 from the start of the
+    // descriptor itself.
+    //
+    // Descriptor length is 12.
+    //
+    // The template parameter value defines 
+    // the maximum length of the descriptor. 
+    // and, therefore, governs its size 
+    // (depending on the build variant).
+    // Size of data is 24
 
-	// Replacing text which has a length > 16
-	// causes panic !!
-	// 
-	// Remove the "//" marks on the next two lines
-	// to see this happen
-	//_LIT(KTxtRepTextPanic,"Text replacement causes panic");
-	//bufc2 = KTxtRepTextPanic;
+    show_tbuf_content_desandcontent_address_len_size_maxlen(buf);
 
-
-	// The Des() function returns a TPtr to the
-	// TBufC.
-	// The TBufC data can be changed through 
-	// the TPtr.
-	// The maximum length of the TPtr is the 
-	// value of the TBufC template parameter,
-	// i.e. 16 
-
-	bufc2 = KTxtHelloWorld;
-	show_bufc3(bufc2);
-
-	TPtr ptr = bufc2.Des();
-	show_ptr(ptr);
-	// Now change the TBufC data through
-	// the TPtr. This is OK provided the length
-	// of the changed data does not exceed 16.
-	//
-	// The following change deletes the last 
-	// character (the "!") and appends 
-	// the characters " & Hi".
-	//
-	// Note that the length of both the TBufC 
-	// and the TPtr reflect the changed data.
-	_LIT(KTxtAndHi," & Hi");
-	ptr.Delete((ptr.Length() - 1), 1);
-	ptr.Append(KTxtAndHi);
-
-	show_bufc3(bufc2);
-	show_ptr(ptr);
-	// * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	// * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	_LIT(KTxtBasicConcepts,"\n-->TBuf basic concepts");
-	console->Printf(KTxtBasicConcepts);
-	_LIT(KTxtPressToContinue," (press any key to continue)\n");
-	console->Printf(KTxtPressToContinue);
-	console->Getch();
-
-	// Construct a TBuf using a Literal			   
-	TBuf<16> buf(KTxtHelloWorld);
-
-	// Look at: 
-	//   1. Descriptor content
-	//   2. Address of descriptor
-	//   3. Address of descriptor data area
-	//   4. Length of descriptor
-	//   5. Size of descriptor
-	//   6. Maximum length of descriptor      
-	//
-	// Like a TBufC, the address of the descriptor
-	// data area is offset 4 from the start of the
-	// descriptor itself.
-	//
-	// Descriptor length is 12.
-	//
-	// The template parameter value defines 
-	// the maximum length of the descriptor. 
-	// and, therefore, governs its size 
-	// (depending on the build variant).
-	// Size of data is 24
-
-	console->Printf(KCommonFormat2, &buf);
-	console->Printf(KCommonFormat3, &buf, buf.Ptr());
-	console->Printf(KCommonFormat9, buf.Length(), buf.Size());
-	console->Printf(KCommonFormat10, buf.MaxLength());
-
-	// The data can be modified
-	buf.Append('@');
-	console->Printf(KCommonFormat2, &buf);
-	console->Printf(KCommonFormat9, buf.Length(), buf.Size());
-	console->Printf(KCommonFormat10, buf.MaxLength());
-
-	// Length can be changed; data represented
-	// by the descriptor is now "Hel"
-	buf.SetLength(3);
-	console->Printf(KCommonFormat2, &buf);
-	console->Printf(KCommonFormat9, buf.Length(), buf.Size());
-	console->Printf(KCommonFormat10, buf.MaxLength());
-
-	// Length can be zeroised; no data is now 
-	// represented by the descriptor but 
-	// the maximum length is still 16
-	buf.Zero();
-	console->Printf(KCommonFormat2, &buf);
-	console->Printf(KCommonFormat9, buf.Length(), buf.Size());
-	console->Printf(KCommonFormat10, buf.MaxLength());
-
-	// The data can be replaced entirely 
-	// using the assignment operator.
-	// The replacement text must not have a
-	// length greater than 16.
-	buf = KTxtRepText;
-	console->Printf(KCommonFormat2, &buf);
-	console->Printf(KCommonFormat9, buf.Length(), buf.Size());
-	console->Printf(KCommonFormat10, buf.MaxLength());
-
-	// Replacing text which has a length > 16
-	// causes panic !!
-	// 
-	// Remove the "//" marks on the next two lines
-	// to see this happen
-	//_LIT(KTxtRepTextPanic,"Text replacement causes panic");
-	//buf = _L("Text replacement causes panic!");
-	}
+    example_tbuf_append_tbuf(buf);
+    example_tbuf_setlength(buf);
+    example_tbuf_zero(buf);
+    example_tbuf_replace_with_lit_normal(buf);
+    example_tbuf_replace_with_lit_panic(buf);
+    }
 
